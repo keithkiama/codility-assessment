da21c6f702eddb52cf1f37e289866a9f008cc4a3
keithkiama <keithkiama2003@gmail.com>
2023-02-23 15:48:37 +0300

    Completed First Code Challenge

--- no_ifs_no_buts.rb
+++ no_ifs_no_buts.rb
@@ -1,6 +1,6 @@
 def no_ifs_no_buts(a, b)
     message = ["smaller than", "equal to", "bigger than"]
       result = (a > b) && 2 || ((a == b) && 1 || 0)
-    return message[result]
+    puts message[result]
 end
-puts no_ifs_no_buts(10,2)
+puts no_ifs_no_buts(10, 2)
